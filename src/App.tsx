19eb7f287c97303bee892b104c463cae7bd53b9a
Correy Lim <correylim@gmail.com>
2023-08-25 17:33:40 -0700

    Randomly select a value and store it in a variable
    To be used when a Value component is implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import { useState } from 'react'
+import {
+  useEffect,
+  useState,
+} from 'react'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import valuesJson from './data/values-and-descriptors.json';
 import './App.css'
+
+type ValuesAndDescriptors = {
+  [value: string]: string;
+}
 
 const darkTheme = createTheme({
   palette: {
@@ -11,7 +18,17 @@ const darkTheme = createTheme({
 });
 
 function App() {
-  const [ values ] = useState(valuesJson);
+  const [ values ] = useState<ValuesAndDescriptors>(valuesJson);
+  /**
+   * @todo Remove random choosing of value once choice and storing is
+   * implemented
+   */
+  const [ randomValue, setRandomValue ] = useState<string>('');
+
+  useEffect(() => {
+    const valuesKeys: Array<string> = Object.keys(values);
+    setRandomValue(valuesKeys[Math.floor(Math.random() * valuesKeys.length)]);
+  }, []);
 
   return (
     <ThemeProvider theme={darkTheme}>
