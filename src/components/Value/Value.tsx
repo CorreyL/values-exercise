01fb761a4aa8c1e14f1bb35b7118d894ee4fe6f9
Correy Lim <correylim@gmail.com>
2023-08-25 20:54:12 -0700

    Add drag & drop logic

--- src/components/Value/Value.tsx
+++ src/components/Value/Value.tsx
@@ -6,17 +6,28 @@ import Typography from '@mui/material/Typography';
 interface ValueProps {
   value: string;
   descriptor: string;
+  draggable?: boolean;
+  onDragStart?: any;
+  onDragEnd?: any;
 };
 
 export default function Value({
   value,
   descriptor,
+  draggable = false,
+  onDragStart = null,
+  onDragEnd = null,
 }: ValueProps) {
   /**
    * @todo Make fonts look better @_@
    */
   return (
-    <Box sx={{ minWidth: 275 }}>
+    <Box
+      sx={{ minWidth: 275 }}
+      draggable={draggable}
+      onDragStart={onDragStart}
+      onDragEnd={onDragEnd}
+    >
       <Card variant="outlined">
         <CardContent>
           <Typography variant="h5" component="div">
--- src/components/ValuesColumn/ValuesColumn.tsx
+++ src/components/ValuesColumn/ValuesColumn.tsx
@@ -1,20 +1,68 @@
+import { DragEvent, useEffect } from 'react';
 import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
+import Value from '../Value';
+import { ValuesAndDescriptors } from '../../App';
 
 interface ValuesColumnProps {
   columnTitle: string,
-  // values: Array<Object>,
+  columnSetter: React.Dispatch<React.SetStateAction<ValuesAndDescriptors>>,
+  values: ValuesAndDescriptors,
 };
 
 export default function ValuesColumn({
   columnTitle,
-  // values,
+  columnSetter,
+  values,
 }: ValuesColumnProps) {
+  useEffect(() => {}, [values]);
   return (
-    <Grid md={4}>
+    <Grid
+      md={4}
+      onDragOver={(event) => {
+        event.preventDefault();
+      }}
+      onDrop={(event) => {
+        event.dataTransfer.setData('droppableZone', 'true');
+        const value = event.dataTransfer.getData('value');
+        const descriptor = event.dataTransfer.getData('descriptor');
+        columnSetter(state => ({
+          ...state,
+          [value]: descriptor,
+        }));
+      }}
+    >
       <Typography variant="h4" component="div">
         {columnTitle}
       </Typography>
+      {
+        Object.keys(values).map((value, idx) => (
+          <Value
+            key={`${columnTitle}-${value}-${idx}`}
+            value={value}
+            descriptor={values[value]}
+            draggable
+            onDragStart={(event: DragEvent) => {
+              event.dataTransfer.clearData();
+              event.dataTransfer.setData('value', value);
+              event.dataTransfer.setData('descriptor', values[value]);
+            }}
+            onDragEnd={(event: DragEvent) => {
+              /**
+               * @todo Need to determine if it was actually dropped in a
+               * droppable zone, otherwise it'll just get deleted
+               *
+               * Also need to figure out why the state change doesn't
+               * immediately update the render for this column
+               */
+              columnSetter(state => {
+                delete state[value];
+                return state;
+              });
+            }}
+          />
+        ))
+      }
     </Grid>
   );
 }
