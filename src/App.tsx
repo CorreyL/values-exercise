2b59cd720870166e2835003de9723a15b83fd903
Correy Lim <correylim@gmail.com>
2023-08-25 17:23:58 -0700

    Load Values data into app from JSON

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import valuesJson from './data/values-and-descriptors.json';
 import './App.css'
 
 const darkTheme = createTheme({
@@ -10,6 +11,8 @@ const darkTheme = createTheme({
 });
 
 function App() {
+  const [ values ] = useState(valuesJson);
+
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
