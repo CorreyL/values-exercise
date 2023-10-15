0ce61211254d6ee35abaf4c192bb4b836d6936ae
Correy Lim <correylim@gmail.com>
2023-10-15 09:53:15 -0700

    Port loadProgressFromFile
    Commensurately expand the props for the setters of the different Value
    columns as well

--- src/components/SaveAndLoad/SaveAndLoad.tsx
+++ src/components/SaveAndLoad/SaveAndLoad.tsx
@@ -7,6 +7,10 @@ interface SaveAndLoadProps {
   veryImportantValues: ValuesAndDescriptors;
   importantValues: ValuesAndDescriptors;
   notImportantValues: ValuesAndDescriptors;
+  setVeryImportantValues: React.Dispatch<React.SetStateAction<ValuesAndDescriptors>>;
+  setImportantValues: React.Dispatch<React.SetStateAction<ValuesAndDescriptors>>;
+  setNotImportantValues: React.Dispatch<React.SetStateAction<ValuesAndDescriptors>>;
+  setLockedValues: React.Dispatch<React.SetStateAction<ValuesAndDescriptors>>;
 };
 
 export default function SaveAndLoad(props: SaveAndLoadProps) {
@@ -15,7 +19,38 @@ export default function SaveAndLoad(props: SaveAndLoadProps) {
     veryImportantValues,
     importantValues,
     notImportantValues,
+    setVeryImportantValues,
+    setImportantValues,
+    setNotImportantValues,
+    setLockedValues,
   } = props;
+
+  const loadProgressFromFile = () => {
+    const input = document.createElement('input');
+    input.style.display = 'none';
+    input.type = 'file';
+    document.body.appendChild(input);
+    input.click();
+    input.onchange = (event) => {
+      const fileReader = new FileReader();
+      if (event?.target && (event.target as HTMLInputElement).files?.length) {
+        // Typescript is not recognizing that event and target have
+        // already been null-checked, begrudgingly ts-ignore
+        // @ts-ignore
+        fileReader.readAsText(event.target.files[0], 'UTF-8');
+        fileReader.onload = (event) => {
+          if (event?.target?.result) {
+            const parsedLoadedFile = JSON.parse(event.target.result as string);
+            setVeryImportantValues(parsedLoadedFile[columnKeys.VERY_IMPORTANT_VALUES]);
+            setImportantValues(parsedLoadedFile[columnKeys.IMPORTANT_VALUES]);
+            setNotImportantValues(parsedLoadedFile[columnKeys.NOT_IMPORTANT_VALUES]);
+            setLockedValues(parsedLoadedFile[lockedValuesKey]);
+          }
+          input.remove();
+        }
+      }
+    };
+  };
 
   const saveValuesToLocalStorage = () => {
     localStorage.setItem(columnKeys.VERY_IMPORTANT_VALUES, JSON.stringify(veryImportantValues));
